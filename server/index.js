3a38bbd3befc45ef7d10fe95acefb5b74ceee19f
Iliana Bobeva <iliana.bobeva@gmail.com>
2017-07-22 21:42:27 +0300

    add dotenv for API key configuration

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8081;
 
-app.get("/", function (req, res) {
-	res.send("Working!!");
+require('dotenv').config();
+
+app.get('/', function (req, res) {
+	res.send('Working!!');
 });
 
 app.listen(port);
-console.log("API is running on port: " + port);
+console.log('API is running on port: ' + port);
