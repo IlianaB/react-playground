6c66bdb1a3e5aded085719c277bee3914d795c58
Iliana Bobeva <iliana.bobeva@gmail.com>
2017-07-27 23:42:23 +0300

    allow cors

--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ var movies = require('./routes/movies');
 
 require('dotenv').config();
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.get('/', function (req, res) {
 	res.send('Working!!');
 });
