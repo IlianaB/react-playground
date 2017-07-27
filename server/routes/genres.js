c2dbd33d59a313a90ecbf29816e5a463f627cda9
Iliana Bobeva <iliana.bobeva@gmail.com>
2017-07-28 00:24:11 +0300

    add axios to the server project

--- server/routes/genres.js
+++ server/routes/genres.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
+var axios = require('axios');
 require('dotenv').config();
 
 var BASE_URL = 'https://api.themoviedb.org/3/';
 var API_KEY = '?api_key=' + process.env.API_KEY;
 
 router.get('/list', function (req, res) {
-	request(BASE_URL + 'genre/movie/list' + API_KEY, function (err, resp, body) {
-		res.json(JSON.parse(body));
-	});
+	axios(BASE_URL + 'genre/movie/list' + API_KEY)
+		.then(function (response) {
+			res.json(response.data);
+		});
 });
 
 router.get('/:id', function (req, res) {
-	request(BASE_URL + 'genre/' + req.params.id + '/movies' + API_KEY, function (err, resp, body) {
-		res.json(JSON.parse(body));
-	})
+	axios(BASE_URL + 'genre/' + req.params.id + '/movies' + API_KEY)
+		.then(function (response) {
+			res.json(response.data);
+		})
 });
 
 module.exports = router;
--- server/routes/movies.js
+++ server/routes/movies.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
+var axios = require('axios');
 require('dotenv').config();
 
 var BASE_URL = 'https://api.themoviedb.org/3/';
 var API_KEY = '?api_key=' + process.env.API_KEY;
 
 router.get('/:id', function (req, res) {
-	request(BASE_URL + 'movie/' + req.params.id + API_KEY, function (err, resp, body) {
-		res.json(JSON.parse(body));
-	})
+	axios(BASE_URL + 'movie/' + req.params.id + API_KEY)
+		.then(function (response) {
+			res.json(response.data);
+	});
 });
 
 module.exports = router;
