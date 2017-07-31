d82ba1759f05d0dc3cc35c2196bb30621a7321e3
Iliana Bobeva <iliana.bobeva@gmail.com>
2017-07-31 21:53:09 +0300

    get all movies in a genre

--- client/src/components/Genre/Genre.js
+++ client/src/components/Genre/Genre.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Genre.css';
 
 class Genre extends Component {
+  constructor() {
+    super();
+    this.state = { movies: [] };
+  }
+  componentDidMount() {
+    this.getMovies(this.props.match.params.id);
+  }
+
+  componentDidUpdate(prevProps) {
+    const id = this.props.match.params.id;
+    const prevId = prevProps.match.params.id;
+
+    if (id !== prevId) {
+      this.getMovies(id);
+    }
+  }
+
   render() {
-    const params = this.props.match.params;
     return (
-        <div>{params.id}</div>
+        <ul>
+            {this.state.movies.map(movie =>
+              <li key={movie.id}>{movie.title}</li>
+            )}
+        </ul>
     );
+  }
+
+  getMovies(genreId) {
+    return axios("http://localhost:8081/genre/" + genreId)
+      .then(response =>
+          this.setState({ movies: response.data.results })
+      );
   }
 }
 
